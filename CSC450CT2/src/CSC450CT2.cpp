de5a086542c3dfe4262971e67137d785eb3b16ae
adamgorry <adamgorry@mac-1786.lan>
2025-10-27 20:21:33 -0600

    Cleaned up printing result with another line, comments for clarity

--- CSC450CT2/src/CSC450CT2.cpp
+++ CSC450CT2/src/CSC450CT2.cpp
@@ -11,25 +11,25 @@
 using namespace std;
 
 int main() {
-	string firstString, secondString, combinedString;
+	string firstString, secondString, combinedString;  //Variables to hold user input and combined result
 
 	cout << "With only magic I will combine two strings, three times! \n" << endl;
 
-	for(int i = 1; i <= 3; i++) {
-		cout << "Loop " << i << endl;
+	for(int i = 1; i <= 3; i++) {  //Loop to get user input 3 times
+		cout << "Loop " << i << endl;  //Print which loop we're on
 
-		cout << "Please enter your first string: ";
+		cout << "Please enter your first string: ";  //Get and store first string
 		getline(cin, firstString);
 
-		cout << "Please enter your second string: ";
+		cout << "Please enter your second string: ";  //Get and store second string
 		getline(cin, secondString);
 
-		combinedString = firstString + secondString;
+		combinedString = firstString + secondString;  //Combine two strings and store it
 
-		cout << "Do you remember your strings?  Here they are magically combined! " << combinedString << endl << endl;
+		cout << "Do you remember your strings?  Here they are magically combined! " << combinedString << endl << endl;  //Print result to console
 	}
 
 	cout << "Thank you for coming to the show!" << endl;
 
-	return 0;
+	return 0;  //End program
 }
