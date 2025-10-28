f0a01f23b5b96d5b828a09ab15191a173b8577ad
adamgorry <adamgorry@mac-1786.lan>
2025-10-27 20:14:33 -0600

    Prompt the user to write two strings, combined them and print result to console

--- CSC450CT2/src/CSC450CT2.cpp
+++ CSC450CT2/src/CSC450CT2.cpp
@@ -15,8 +15,21 @@ int main() {
 
 	cout << "With only magic I will combine two strings, three times! \n" << endl;
 
-	for (int i = 1; i <= 3; i++) {
+	for(int i = 1; i <= 3; i++) {
 		cout << "Loop " << i << endl;
+
+		cout << "Please enter your first string: ";
+		getline(cin, firstString);
+
+		cout << "Please enter your second string: ";
+		getline(cin, secondString);
+
+		combinedString = firstString + secondString;
+
+		cout << "Do you remember your strings?  Here they are magically combined! " << combinedString << endl << endl;
+	}
+
+	cout << "Thank you for coming to the show!" << endl;
 
 	return 0;
 }
