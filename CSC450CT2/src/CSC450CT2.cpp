713fbf0d56ecf7d8462480e7656d46d6ad32bdea
adamgorry <adamgorry@mac-1786.lan>
2025-10-27 19:13:22 -0600

    Initial commit, project start

---
+++ CSC450CT2/src/CSC450CT2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : CSC450CT2.cpp
+// Author      : Adam Gorry
+// Version     :
+// Copyright   : Your copyright notice
+// Description : CSC450 Critical Thinking 2
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
